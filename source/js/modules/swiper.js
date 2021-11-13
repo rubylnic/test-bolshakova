233687a5e69318a804938a43ee43d2e415435bbf
rubylnic <ksenia.bolshako@mail.ru>
2021-11-13 12:45:52 +0300

    feat: add resposive to index

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -1,9 +1,11 @@
 'use strict';
 (function() {
+  const faqLeft = document.querySelector('.faq__arrow-left');
+  const faqRight = document.querySelector('.faq__arrow-right');
 
   const faqSwiper = new Swiper('.faq__container', {
     slidesPerView: 1,
-    loop: true,
+    // loop: true,
     spaceBetween: 24,
 
     breakpoints: {
@@ -19,5 +21,21 @@
         slidesPerView: 4,
       }
     },
+    navigation: {
+      nextEl: '.faq__arrow-right',
+      prevEl: '.faq__arrow-left',
+    },
+
   });
+
+  faqSwiper.on('slideChange', function() {
+    if (faqSwiper.realIndex == 0) {
+      faqLeft.classList.add('hidden');
+    } else if (faqSwiper.isEnd) {
+      faqRight.classList.add('hidden');
+    } else {
+      faqRight.classList.remove('hidden');
+      faqLeft.classList.remove('hidden');
+    }
+  })
 })();
